0b02da02c8bdb5bdfd1d4c3c9c42886d88ecf5f1
Anais Di Genua <ACC_ADIGENUA@ntvspa.it>
2025-12-07 19:18:51 +0100

    feat: add navbar

---
+++ src/components/header/navbar.tsx
@@ -0,0 +1,13 @@
+import "./navbar.css";
+
+export default function Nav() {
+  return (
+    <>
+      <nav className="navbar">
+        <a>Home</a>
+        <a>Login</a>
+        <a>Registrati</a>
+      </nav>
+    </>
+  );
+}
