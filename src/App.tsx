9831ed46f3b998b1de06cc9a787c8065969be51c
Anais Di Genua <ACC_ADIGENUA@ntvspa.it>
2025-12-07 19:29:40 +0100

    feat: add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./components/header/navbar";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <>
       <Nav />
+      <Home />
     </>
   );
 }
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,15 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className="home-wrapper">
+        <h1>Titolo placeholder</h1>
+        <p>App per gestire il tuo utente e i tuoi post</p>
+        <div>
+          Fai il login per vedere i dettagli del tuo profilo oppure registrati
+        </div>
+      </div>
+    </>
+  );
+}
