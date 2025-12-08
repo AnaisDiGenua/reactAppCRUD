5e58a492bc9a02c53ddf8f6129ede4a8ebd1a6bd
Anais Di Genua <ACC_ADIGENUA@ntvspa.it>
2025-12-08 18:05:18 +0100

    feat: refactoring navbar with userContext

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
+import { useUserContext } from "../../context/userContext";
 import "./navbar.css";
 
 export default function Nav() {
+  const { user } = useUserContext();
+
   return (
     <>
       <nav className="navbar">
         <Link to="/">Home</Link>
-        <a>Login</a>
-        <Link to="/user-registration">Registrati</Link>
-        <Link to="/user-detail">Account</Link>
+        {!user && (
+          <>
+            <a>Login</a>
+            <Link to="/user-registration">Registrati</Link>
+          </>
+        )}
+        {user && (
+          <>
+            <Link to={`/user-detail/${user.id}`}>Account</Link>
+          </>
+        )}
       </nav>
+      ;
     </>
   );
 }
