02112f54cf88cef131b9f80522a90cc0e6aedce1
Anais Di Genua <ACC_ADIGENUA@ntvspa.it>
2025-12-09 19:42:15 +0100

    fix: delete home.css and refctoring some style

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,24 +1,25 @@
 import { useUserContext } from "../../context/userContext";
 import { Link } from "react-router-dom";
-import "./home.css";
 
 export default function Home() {
   const { user } = useUserContext();
   return (
     <>
-      <div className="home-wrapper">
-        <h1>React App</h1>
-        <p>App per gestire il tuo utente e i tuoi post</p>
+      <div className="box text-center">
+        <h1 className="text-3xl font-bold">React App</h1>
+        <p className="my-3">App per gestire il tuo utente e i tuoi post</p>
 
         {user ? (
-          <div className="home-logged">
-            <h2>Ciao {user.name}!</h2>
-            <p>Puoi accedere ai dettagli del tuo account qui sotto.</p>
+          <>
+            <h2 className="mt-6">Ciao {user.name}!</h2>
+            <p className="mb-6">
+              Puoi accedere ai dettagli del tuo account qui sotto.
+            </p>
 
-            <Link className="button" to={`/user-detail/${user.id}`}>
+            <Link className="button self-center" to={`/user-detail/${user.id}`}>
               Vai al tuo profilo
             </Link>
-          </div>
+          </>
         ) : (
           <div>
             Fai il login per vedere i dettagli del tuo profilo oppure registrati
